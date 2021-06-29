ce88c765cb0ff8b35eaca6f707197695d8509d9b
simonpforster <simon.forster98@gmail.com>
2021-06-29 15:45:50 +0100

    UserRepoIt tests adjusted to fit here

--- it/repositories/UserRepositoryIt.scala
+++ it/repositories/UserRepositoryIt.scala
@@ -1,6 +1,6 @@
 package repositories
 
-import models.User
+import models.{EncryptedPassword, User}
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
@@ -8,6 +8,7 @@ import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 import play.api.Application
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test.Helpers.{await, defaultAwaitTimeout}
+import service.EncryptionService
 import uk.gov.hmrc.mongo.test.DefaultPlayMongoRepositorySupport
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -22,14 +23,21 @@ class UserRepositoryIt extends AnyWordSpec with GuiceOneServerPerSuite
 	override implicit lazy val app: Application = new GuiceApplicationBuilder()
 		.configure().build()
 
-	private val testUser = User("testCrn", "testPass")
+	val crypto: EncryptionService = app.injector.instanceOf[EncryptionService]
+	val nonce: Array[Byte] = crypto.getNonce
+	val testPass = "testPass"
+	val ePassword = new EncryptedPassword(crypto.encrypt(testPass.getBytes, crypto.getKey, nonce), nonce)
+
+	private val testUser = User("testCrn", ePassword)
 
 	"UserRepository" can {
 		"read" should {
 			"succeed" in {
 				await(repository.create(testUser))
 
-				await(repository.read("testCrn")) shouldBe Some(testUser)
+				val something = await(repository.read("testCrn")).get
+				something.crn shouldBe testUser.crn
+				crypto.decrypt(something.password.ePassword, crypto.getKey, something.password.nonce) shouldBe "testPass"
 			}
 
 			"fail because of not found" in {
